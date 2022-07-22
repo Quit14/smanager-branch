0bc9e71559b98b9043cb2ccc88755f37b92599ed
Anastasiya Volkova <ahh.write@gmail.com>
2022-07-22 21:31:48 +0300

    initial comit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[] {125, 20, 6, 14});
+        System.out.println(salesManager.max());
+    }
+}
